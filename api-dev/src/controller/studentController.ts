54dc981b8a3573953c8603f508b98b75135db36e
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-05-21 19:13:58 +0630

    add necessary comments

--- api-dev/src/controller/studentController.ts
+++ api-dev/src/controller/studentController.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { Student } from '../model/Student';
 
+// return value ပြန်စရာမရှိလို့ void ကိုသံုးတယ်
+// Promise ကိုသံုးတာက typescript ကိုမှ async function မို့လို့
 export const getAllStudent = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
         const allStudents = await Student.find();
@@ -12,7 +14,7 @@ export const getAllStudent = async (req: express.Request, res: express.Response)
 
 export const getStudent = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        const { id } = req.params;
+        const { id } = req.params; // id က route url မှာပါေနလို့ req.params ကိုသံုးမယ်
         const student = await Student.findById(id);
         if (!student) {
             res.status(404).json({ message: 'Student not found' });
@@ -26,14 +28,14 @@ export const getStudent = async (req: express.Request, res: express.Response): P
 
 export const addStudent = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        const { name, email, mobile, dob } = req.body;
+        const { name, email, mobile, dob } = req.body; //user input ကိုယူရမှာမလို့ req.bodyကိုသံုးမယ်
         const student = new Student({
             name,
             email,
             mobile,
             dob,
         });
-        await student.save();
+        await student.save(); //database ထဲ သိမ်းမယ်
         res.status(201).json({ message: "Student info successfully added!", data: student });
     } catch (error) {
         res.status(400).json(error);
@@ -42,12 +44,12 @@ export const addStudent = async (req: express.Request, res: express.Response): P
 
 export const updateStudent = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        const { id } = req.params;
-        const { name, email, mobile, dob } = req.body;
+        const { id } = req.params; // id က route url မှာပါေနလို့ req.params ကိုသံုးမယ်
+        const { name, email, mobile, dob } = req.body; //user input ကိုယူရမှာမလို့ req.bodyကိုသံုးမယ်
         const student = await Student.findByIdAndUpdate(
-            id,
-            { name, email, mobile, dob },
-            { new: true }
+            id, // ဒီ ​​id က req.params က id
+            { name, email, mobile, dob }, // update လုပ်မဲ့ ​obj အသစ်
+            { new: true } // ဒါမပါရင် ​update မဖြစ်ပဲ original ဟာပဲပြန်ဖြစ်မယ်
         );
         if (!student) {
             res.status(404).json({ message: 'Student not found' });
@@ -61,7 +63,7 @@ export const updateStudent = async (req: express.Request, res: express.Response)
 
 export const deleteStudent = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        const { id } = req.params;
+        const { id } = req.params; // id က route url မှာပါေနလို့ req.params ကိုသံုးမယ်
         const student = await Student.findByIdAndDelete(id);
         if (!student) {
             res.status(404).json({ message: 'Student not found' });
